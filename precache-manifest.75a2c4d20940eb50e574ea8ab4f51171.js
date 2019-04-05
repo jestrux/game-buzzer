0256cbb3ca5d89e21b824e7117b8dcb6bda7e866
Walter Kimaro <wakyj07@gmail.com>
2019-04-05 08:50:11 +0300

    initial setup

---
+++ precache-manifest.75a2c4d20940eb50e574ea8ab4f51171.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1704089d93c284a422",
+    "url": "/static/css/main.f053f3a8.chunk.css"
+  },
+  {
+    "revision": "4e1704089d93c284a422",
+    "url": "/static/js/main.4e170408.chunk.js"
+  },
+  {
+    "revision": "f35b02e737d897d8ea3c",
+    "url": "/static/js/1.f35b02e7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a5ffd5ca24afb6a42eac46276bb8f9e0",
+    "url": "/index.html"
+  }
+];
